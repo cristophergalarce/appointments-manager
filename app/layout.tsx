3507214e526bc8f437536d9f5022293077423a25
cristring <crstphx@gmail.com>
2024-07-26 21:29:50 -0500

    add: clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col min-h-screen max-w-screen-xl mx-auto`}>{children}</body>
+      <body className={`${inter.className} flex flex-col min-h-screen max-w-screen-xl mx-auto`}>{children}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ndhkc7irxa");
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
